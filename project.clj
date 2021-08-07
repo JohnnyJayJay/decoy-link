7b5eea405216889d4210377d88e156c8cc729e2e
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-08-07 18:30:56 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject decoy-link "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Create redirects with custom link previews"
+  :url "https://github.com/JohnnyJayJay/decoy-link"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
@@ -10,7 +10,9 @@
                  [selmer "1.12.44"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.12.5"]]
+  :aot :all
   :ring {:handler decoy-link.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]]
+         :aot nil}})
